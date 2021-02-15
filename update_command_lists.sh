dbd93370641467cfbee15592c94f1ad8eb6322d3
Henrik Grimler <henrik@grimler.se>
2021-02-15 16:00:16 +0100

    update_command_lists: use lowercase for variables

--- update_command_lists.sh
+++ update_command_lists.sh
@@ -4,19 +4,19 @@ shopt -s nullglob
 set -e
 
 if [ "$1" == "all" ]; then
-    REPOS="termux-packages termux-root-packages science-packages game-packages unstable-packages x11-packages"
+    repos="termux-packages termux-root-packages science-packages game-packages unstable-packages x11-packages"
 else
-    REPOS="$1"
+    repos="$1"
 fi
 
-: ${TMPDIR:=/tmp}
+: "${TMPDIR:=/tmp}"
 
 download_deb() {
     # This function sources a package's build.sh, and possible *.subpackage.sh,
     # and downloads the debs from a given repo. Debs are saved in $TERMUX_TOPDIR/_cache-$ARCH,
     # which is the same directory as when doing ./build-package.sh -i <pkg> builds
-    PKG=$1
-    PKG_DIR=$2
+    pkg=$1
+    pkg_dir=$2
     TERMUX_ARCH=$3
 
     # TERMUX_TOPDIR is defined in termux_step_setup_variables
@@ -25,106 +25,106 @@ download_deb() {
     export TERMUX_SCRIPTDIR=./termux-packages/termux-packages
     source $TERMUX_SCRIPTDIR/scripts/properties.sh
     termux_step_setup_variables
-    cd $REPO/$REPO
+    cd $repo/$repo
     export TERMUX_SCRIPTDIR=.
 
-    for BUILD_FILE in ${PKG_DIR}/build.sh ${PKG_DIR}*.subpackage.sh; do
-        if [ "$(basename $BUILD_FILE)" == "build.sh" ]; then
-            PKG_NAME=$PKG
+    for build_file in ${pkg_dir}/build.sh ${pkg_dir}*.subpackage.sh; do
+        if [ "$(basename $build_file)" == "build.sh" ]; then
+            pkg_name=$pkg
         else
-            PKG_NAME=$(basename ${BUILD_FILE%%.*})
+            pkg_name=$(basename ${build_file%%.*})
         fi
         # Assume arch=TERMUX_ARCH unless TERMUX_{,SUB}PKG_PLATFORM_INDEPENDENT=true
         export TERMUX_ARCH
-        DEP_ARCH=""
-        DEP_VERSION=""
+        dep_arch=""
+        dep_version=""
 
         # Some packages, like all of texlive's subpackages, gives an error when sourcing the build.sh.
         # This happens because texlive's subpackages use a script to get the file list, which fails due
         # to unset variables in this context. We are only interested in the arch, not the file list
         # though so this error is not blocking. stderr is redirected to /dev/null below until a nicer
         # workaround can be found.
-        read DEP_ARCH DEP_VERSION <<< $(termux_extract_dep_info $PKG_NAME "${PKG_DIR}" 2>/dev/null)
-        if [ -z "$DEP_ARCH" ]; then
+        read dep_arch dep_version <<< $(termux_extract_dep_info $pkg_name "${pkg_dir}" 2>/dev/null)
+        if [ -z "$dep_arch" ]; then
             # termux_extract_dep_info returned nothing so the package is
             # probably blacklisted for the current arch
             return
         fi
         (
-            mkdir -p "$TERMUX_TOPDIR/_cache-${DEP_ARCH}"
-            cd "$TERMUX_TOPDIR/_cache-${DEP_ARCH}"
-            if [ ! -f "${PKG_NAME}_${DEP_VERSION}_${DEP_ARCH}.deb" ];
+            mkdir -p "$TERMUX_TOPDIR/_cache-${dep_arch}"
+            cd "$TERMUX_TOPDIR/_cache-${dep_arch}"
+            if [ ! -f "${pkg_name}_${dep_version}_${dep_arch}.deb" ];
             then
-                echo "Downloading ${REPO_URL}/$DEP_ARCH/${PKG_NAME}_${DEP_VERSION}_${DEP_ARCH}.deb" 1>&2
-                TEMP_DEB=$(mktemp $TMPDIR/${PKG_NAME}_${DEP_VERSION}_${DEP_ARCH}.deb.XXXXXX)
-                curl --fail -L -o "${TEMP_DEB}" "${REPO_URL}/$DEP_ARCH/${PKG_NAME}_${DEP_VERSION}_${DEP_ARCH}.deb" || exit 1
-                mv ${TEMP_DEB} ${PKG_NAME}_${DEP_VERSION}_${DEP_ARCH}.deb
+                echo "Downloading ${repo_url}/$dep_arch/${pkg_name}_${dep_version}_${dep_arch}.deb" 1>&2
+                temp_deb=$(mktemp $TMPDIR/${pkg_name}_${dep_version}_${dep_arch}.deb.XXXXXX)
+                curl --fail -L -o "${temp_deb}" "${repo_url}/$dep_arch/${pkg_name}_${dep_version}_${dep_arch}.deb" || exit 1
+                mv ${temp_deb} ${pkg_name}_${dep_version}_${dep_arch}.deb
             else
-                printf "%-50s %s\n" "${PKG_NAME}_${DEP_VERSION}_${DEP_ARCH}.deb" "already downloaded" 1>&2
+                printf "%-50s %s\n" "${pkg_name}_${dep_version}_${dep_arch}.deb" "already downloaded" 1>&2
             fi
-            echo "$TERMUX_TOPDIR/_cache-${DEP_ARCH}/${PKG_NAME}_${DEP_VERSION}_${DEP_ARCH}.deb\n"
+            echo "$TERMUX_TOPDIR/_cache-${dep_arch}/${pkg_name}_${dep_version}_${dep_arch}.deb\n"
         )
     done
 }
 
-for REPO in $REPOS; do
-    case $REPO in
+for repo in $repos; do
+    case $repo in
         termux-packages)
-            REPO_URL="https://dl.bintray.com/termux/$REPO-24"
+            repo_url="https://dl.bintray.com/termux/$repo-24"
             ;;
         termux-root-packages)
-            REPO_URL="https://dl.bintray.com/grimler/$REPO-24"
+            repo_url="https://dl.bintray.com/grimler/$repo-24"
             ;;
         science-packages)
-            REPO_URL="https://dl.bintray.com/grimler/$REPO-24"
+            repo_url="https://dl.bintray.com/grimler/$repo-24"
             ;;
         game-packages)
-            REPO_URL="https://dl.bintray.com/grimler/$REPO-24"
+            repo_url="https://dl.bintray.com/grimler/$repo-24"
             ;;
         unstable-packages)
-            REPO_URL="https://dl.bintray.com/xeffyr/$REPO"
+            repo_url="https://dl.bintray.com/xeffyr/$repo"
             ;;
         x11-packages)
-            REPO_URL="https://dl.bintray.com/xeffyr/$REPO"
+            repo_url="https://dl.bintray.com/xeffyr/$repo"
             ;;
         *)
-            echo "Unknown repo: '$REPO'"
+            echo "Unknown repo: '$repo'"
             exit 1
     esac
 
-    for ARCH in aarch64 arm i686 x86_64; do
+    for arch in aarch64 arm i686 x86_64; do
         # Get current commit, based on files checked into git
-        CURRENT_COMMIT=$(basename $(git ls-files $REPO/commands-${ARCH}-*.h) \
+        current_commit=$(basename $(git ls-files $repo/commands-${arch}-*.h) \
                              |awk -F"-" '{ print substr($3,1,7) }')
 
         # Get new commit (current checked out commit of submodule)
-        NEW_COMMIT=$(git submodule status $REPO \
+        new_commit=$(git submodule status $repo \
                          |awk '{ if ($1 ~ /^+/) {print substr($1,2,7)} else {print substr($1,1,7)} }')
-        if [ "$CURRENT_COMMIT" == "$NEW_COMMIT" ]; then continue; fi
+        if [ "$current_commit" == "$new_commit" ]; then continue; fi
 
-        UPDATED_PACKAGES=$(cd $REPO/$REPO;
+        updated_packages=$(cd $repo/$repo;
                            git diff --dirstat=files,0 \
-                               ${CURRENT_COMMIT}..${NEW_COMMIT} \
+                               ${current_commit}..${new_commit} \
                                -- packages|awk '{if (gsub(/\//, "/") == 2) print $2}')
-        DEBS=""
-        DELETED_PACKAGES=""
-        for PACKAGE in ${UPDATED_PACKAGES}; do
-            if [ -d $REPO/$REPO/$PACKAGE ] && ! grep -q $ARCH < <(grep "^TERMUX_PKG_BLACKLISTED_ARCHES=" $REPO/$REPO/$PACKAGE/build.sh); then
-                DEBS+="$(download_deb $(basename $PACKAGE) $PACKAGE $ARCH)"
+        debs=""
+        deleted_packages=""
+        for package in ${updated_packages}; do
+            if [ -d $repo/$repo/$package ] && ! grep -q $arch < <(grep "^TERMUX_PKG_BLACKLISTED_ARCHES=" $repo/$repo/$package/build.sh); then
+                debs+="$(download_deb $(basename $package) $package $arch)"
             else
                 # Package seem to have been deleted,
                 # we need to delete it from the command list
-                DELETED_PACKAGES+=" $PACKAGE"
+                deleted_packages+=" $package"
             fi
         done
-        if [ ! "$DELETED_PACKAGES" == "" ]; then
-            EXTRA_ARGS="--delete $DELETED_PACKAGES"
+        if [ ! "$deleted_packages" == "" ]; then
+            extra_args="--delete $deleted_packages"
         fi
 
         # Length of $DEBS could be larger than ARG_MAX, at least on some
         # systems. To not risk such a problem we pipe the DEB list instead of
         # giving it as an argument.
-        echo -e "$DEBS" | ./modify_command_list.py "./${REPO}/commands-${ARCH}-${CURRENT_COMMIT}.h" ${NEW_COMMIT} ${EXTRA_ARGS}
-        sed -i "s%# include \"${REPO}/commands-${ARCH}-.*.h\"%# include \"${REPO}/commands-${ARCH}-${NEW_COMMIT}\.h\"%g" command-not-found.cpp
+        echo -e "$debs" | ./modify_command_list.py "./${repo}/commands-${arch}-${current_commit}.h" ${new_commit} ${extra_args}
+        sed -i "s%# include \"${repo}/commands-${arch}-.*.h\"%# include \"${repo}/commands-${arch}-${new_commit}\.h\"%g" command-not-found.cpp
     done
 done
