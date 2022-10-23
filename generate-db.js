6a32eafa06dd742ad085dd06f244d140fd513705
segfault-bilibili <segfault_bilibili@qq.com>
2022-10-23 17:59:52 +0800

    print status to console

--- generate-db.js
+++ generate-db.js
@@ -20,57 +20,84 @@ const binPrefix = prefix.substring(1) + "/bin/";
 const repoBuffer = fs.readFileSync(path.join(scriptdir, "repo.json"));
 const repoJSON = JSON.parse(repoBuffer);
 
+const fetchFile = (url) => new Promise((resolve, reject) => {
+  let req = https.get(url);
+  req.on("error", (err) => {
+    req.destroy();
+    reject(err);
+  });
+
+  req.on("response", (res) => {
+    if (res.statusCode != 200) {
+      req.destroy(); res.destroy();
+      reject(new Error(`${url} returned ${res.statusCode}`));
+    }
+    res.on("error", (err) => {
+      req.destroy(); res.destroy();
+      reject(err);
+    });
+
+    let rawData = [];
+    res.on("data", (chunk) => {
+      rawData.push(chunk);
+    });
+    res.on("end", () => {
+      req.destroy(); res.destroy();
+      resolve(Buffer.concat(rawData));
+    });
+  });
+});
+
+const promises = [];
 Object.keys(repoJSON).forEach((repo_path) => {
   const repo = repoJSON[repo_path];
   archs.forEach((arch) => {
-    https.get(
-      `${repositoryURL}/${repo.name}/dists/${repo.distribution}/Contents-${arch}.gz`,
-      (res) => {
-        if (res.statusCode != 200) {
-          throw new Error(`${res.url} returned ${res.statusCode}`);
-        }
-
-        let rawData = [];
-        res.on("data", (chunk) => {
-          rawData.push(Buffer.from(chunk, "binary"));
+    let reqUrl = `${repositoryURL}/${repo.name}/dists/${repo.distribution}/Contents-${arch}.gz`;
+    promises.push(fetchFile(reqUrl).then((rawBuffer) => {
+      let binMap = {};
+      let ungzipped = zlib.gunzipSync(rawBuffer).toString();
+      let linesContainingPathToBinaries = ungzipped
+        .split("\n")
+        .filter((line) => {
+          return line.startsWith(binPrefix);
         });
-        res.on("end", () => {
-          let binMap = {};
-          let rawBuffer = Buffer.concat(rawData);
-          let ungzipped = zlib.gunzipSync(rawBuffer).toString();
-          let linesContainingPathToBinaries = ungzipped
-            .split("\n")
-            .filter((line) => {
-              return line.startsWith(binPrefix);
-            });
-          linesContainingPathToBinaries.forEach((line) => {
-            const [pathToBinary, packageNames] = line.split(" ");
-            const binary = pathToBinary.substring(
-              pathToBinary.lastIndexOf("/") + 1
-            );
-            const packages = packageNames.split(",");
-
-            packages.forEach((packageName) => {
-              if (binMap[packageName] === undefined) {
-                binMap[packageName] = [];
-              }
-              binMap[packageName].push(binary);
-            });
-          });
-          const headerFile = `commands-${arch}-${repo.name}.h`;
-          if (fs.existsSync(headerFile)) {
-            fs.rmSync(headerFile);
+      linesContainingPathToBinaries.forEach((line) => {
+        const [pathToBinary, packageNames] = line.split(" ");
+        const binary = pathToBinary.substring(
+          pathToBinary.lastIndexOf("/") + 1
+        );
+        const packages = packageNames.split(",");
+        packages.forEach((packageName) => {
+          if (binMap[packageName] === undefined) {
+            binMap[packageName] = [];
           }
-          Object.keys(binMap)
-            .sort()
-            .forEach((packageName) => {
-              fs.appendFileSync(headerFile, `"${packageName}",\n`);
-              binMap[packageName].sort().forEach((packageName) => {
-                fs.appendFileSync(headerFile, `" ${packageName}",\n`);
-              });
-            });
+          binMap[packageName].push(binary);
         });
+      });
+      const unjoinedContent = [];
+      Object.keys(binMap)
+        .sort()
+        .forEach((packageName) => {
+          unjoinedContent.push(`"${packageName}",\n`);
+          binMap[packageName].sort().forEach((packageName) => {
+            unjoinedContent.push(`" ${packageName}",\n`);
+          });
+        });
+      const content = unjoinedContent.join("");
+
+      const headerFile = `commands-${arch}-${repo.name}.h`;
+      if (fs.existsSync(headerFile)) {
+        fs.rmSync(headerFile);
       }
-    );
+      const encoding = "utf-8";
+      fs.writeFileSync(headerFile, content, { encoding: encoding });
+      if (fs.readFileSync(headerFile, { encoding: encoding }) !== content) throw new Error(`error writting to ${headerFile}`);
+
+      console.log(`${path.basename(__filename)}: downloaded from ${reqUrl} and then written to ${headerFile}`);
+    }).catch((e) => {
+      console.error(`${path.basename(__filename)}: error during/after downloading from ${reqUrl}`, e);
+      throw e;
+    }));
   });
 });
+Promise.allSettled(promises).then((results) => process.exit(results.find((result) => result.status !== "fulfilled") == null ? 0 : -1));
